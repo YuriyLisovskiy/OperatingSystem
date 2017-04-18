e0f682f3d42f3d937104fd7831139d7ede24ee6f
Yura Lisovskiy <yuralisovskiy98@gmail.com>
2017-04-18 23:20:32 +0300

    improved I/O stream

--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -6,7 +6,6 @@
 // IDT_SIZE = 256
 // INTERRUPT_GATE = 0x8e
 // KERNEL_CODE_SEGMENT_OFFSET = 0x08
-
 // ENTER_KEY_CODE = 0x1C
 
 extern unsigned char keyboardMap[128];
@@ -18,8 +17,8 @@ extern void loadIdt(unsigned long *idtPtr);
 unsigned char keyboardMap[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
+  '9', '0', '-', '=', 0,	/* Backspace */
+  ' ',			/* Tab */
   'q', 'w', 'e', 'r',	/* 19 */
   't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
     0,			/* 29   - Control */
@@ -54,24 +53,20 @@ unsigned char keyboardMap[128] =
     0,	/* F12 Key */
     0,	/* All other keys are undefined */
 };
-
-struct IDTEntry {
+struct IDTEntry
+{
 	unsigned short int offsetLowerbits;
 	unsigned short int selector;
 	unsigned char zero;
 	unsigned char typeAttr;
 	unsigned short int offsetHigherbits;
 };
-
 struct IDTEntry IDT[IDT_SIZE];
-
-
 void input(void)
 {
 	unsigned long keyboardAddress;
 	unsigned long idtAddress;
 	unsigned long idtPtr[2];
-
 	/* populate IDT entry of keyboard's interrupt */
 	keyboardAddress = (unsigned long)keyboardHandler;
 	IDT[0x21].offsetLowerbits = keyboardAddress & 0xffff;
@@ -79,13 +74,11 @@ void input(void)
 	IDT[0x21].zero = 0;
 	IDT[0x21].typeAttr = INTERRUPT_GATE;
 	IDT[0x21].offsetHigherbits = (keyboardAddress & 0xffff0000) >> 16;
-
 	/*     Ports
 	*	 PIC1	PIC2
 	*Command 0x20	0xA0
 	*Data	 0x21	0xA1
 	*/
-
 	/* ICW1 - begin initialization */
 	writePort(0x20 , 0x11);
 	writePort(0xA0 , 0x11);
@@ -97,27 +90,21 @@ void input(void)
 	*/
 	writePort(0x21 , 0x20);
 	writePort(0xA1 , 0x28);
-
 	/* ICW3 - setup cascading */
 	writePort(0x21 , 0x00);
 	writePort(0xA1 , 0x00);
-
 	/* ICW4 - environment info */
 	writePort(0x21 , 0x01);
 	writePort(0xA1 , 0x01);
 	/* Initialization finished */
-
 	/* mask interrupts */
 	writePort(0x21 , 0xff);
 	writePort(0xA1 , 0xff);
-
 	/* fill the IDT descriptor */
 	idtAddress = (unsigned long)IDT ;
 	idtPtr[0] = (sizeof (struct IDTEntry) * IDT_SIZE) + ((idtAddress & 0xffff) << 16);
 	idtPtr[1] = idtAddress >> 16 ;
-
 	loadIdt(idtPtr);
-
 	kbInit();
 	return;
 }
@@ -133,24 +120,41 @@ void keyboardHandlerMain(void)
 {
 	unsigned char status;
 	char keycode;
-
 	/* write EOI */
 	writePort(0x20, 0x20);
-
 	status = readPort(KEYBOARD_STATUS_PORT);
 	/* Lowest bit of status will be set if buffer is not empty */
 	if (status & 0x01) {
 		keycode = readPort(KEYBOARD_DATA_PORT);
 		if(keycode < 0)
-			return;
-
-		if(keycode == ENTER_KEY_CODE) {
-			jumpNewLine();
+		{
 			return;
 		}
-
-		vidPtr[currentPosStr++] = keyboardMap[(unsigned char) keycode];
-		vidPtr[currentPosStr++] = 0x02;
+		else if(keycode == BACKSPACE_KEY_CODE && currentPosStr == 0)
+		{
+			vidPtr[currentPosStr] = ' ';
+		}
+		else if(keycode == ENTER_KEY_CODE)
+		{
+			jumpNewLine();
+		}
+		else if(keycode == BACKSPACE_KEY_CODE)
+		{
+			if((currentPosStr % COLUMNS_IN_LINE != 0))
+			{
+				vidPtr[currentPosStr--] = keyboardMap[(unsigned char) keycode];
+				vidPtr[currentPosStr--] = 0x02;
+			}
+			else
+			{
+				vidPtr[currentPosStr] = ' ';
+			}
+		}
+		else
+		{
+			vidPtr[currentPosStr++] = keyboardMap[(unsigned char) keycode];
+			vidPtr[currentPosStr++] = 0x02;
+		}
 	}
 	return;
 }
--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -2,7 +2,7 @@ bits 32
 section .text
         ;multiboot spec
         align 4
-        dd 0x1BADB002              ;magic
+        dd 0x1BADB002              ;magic number
         dd 0x00                    ;flags
         dd - (0x1BADB002 + 0x00)   ;checksum. m+f+c should be zero
 
@@ -30,7 +30,7 @@ writePort:
 loadIdt:
 	mov edx, [esp + 4]
 	lidt [edx]
-	sti 				;turn on interrupts
+	sti		;turn on interrupts
 	ret
 
 keyboardHandler:                 
@@ -38,10 +38,10 @@ keyboardHandler:
 	iretd
 
 start:
-	cli 				;block interrupts
+	cli 		;block interrupts
 	mov esp, stack_space
 	call kmain
-	hlt 				;halt the CPU
+	hlt 		;halt the CPU
 
 section .bss
 resb 8192; 8KB for stack
